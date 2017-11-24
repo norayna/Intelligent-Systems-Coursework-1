5a68b165be0af747b2127b6c48ae319e8102c2b4
norayna <rb1g16@srv02094.soton.ac.uk>
2017-11-24 17:53:59 +0000

    created Problem class, haven't tested it yet

---
+++ src/Problem.java
@@ -0,0 +1,44 @@
+public class Problem {
+	Grid2 grid;
+	Grid2 solution;
+	//Grid2 startState;
+
+	public Problem(int size, Grid2 solution, Grid2 startState) {
+		//grid = new Grid2(size);
+		grid = startState;	// you just feed an entire grid in as the start state
+		
+		this.solution = solution;
+		this.startState = startState;
+	}
+
+	public boolean isSolved() {
+	boolean isSolved = true;
+		for(i = 0; i < grid.size; i++)
+			for (j = 0; j < grid.size; j++) {
+				if(grid[i][j] != solution[i][j]) {
+					isSolved = false;
+				}
+			}
+		}
+
+		return isSolved;
+	}
+	
+	public void dfs() {
+		// look up pseudocode
+	}
+
+	public void bfs() {
+		// look up pseudocode
+	}
+
+	public void ids() {
+		// look up pseudocode
+	}
+
+	public void aStar() {
+		// look up pseudocode
+	}
+
+
+
